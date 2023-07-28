4ff678a4702151cfacbbafa95483cbab7f8c1f70
Joaquim Firmo <joaquimnt18@gmail.com>
2023-07-28 16:56:06 -0300

    refactor: ajuste no controller de schedule para a nova conexao do banco

--- interfaces/controllers/schedule-controller.js
+++ interfaces/controllers/schedule-controller.js
@@ -6,10 +6,12 @@ const UpdateSchedule = require('../../application/use_cases/schedules/update-sch
 // por enquanto os reposiories serão importados e passados para o use-case no controller
 const ScheduleRepository = require('../../infrastructure/repositories/schedule-repository')
 const Connection = require('../../infrastructure/database/connection')
+const Pool = require('../../infrastructure/database/pool')
 
 class ScheduleController {
   async all() {
-    const connection = new Connection()
+    const pool = Pool.create()
+    const connection = new Connection(pool)
     await connection.connect()
     const schedulesRepository = new ScheduleRepository(connection)
     const allSchedules = new AllSchedules(schedulesRepository)
