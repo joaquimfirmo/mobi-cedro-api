b14e1735edca8fa47278d5d8194967e4d4c73a9c
Joaquim Firmo <joaquimnt18@gmail.com>
2023-08-27 17:18:14 -0300

    schedule-controller

--- interfaces/controllers/schedule-controller.js
+++ interfaces/controllers/schedule-controller.js
@@ -1,9 +1,4 @@
 const AllSchedules = require('../../application/use_cases/schedules/list-all-schedules')
-const FindSchedulesById = require('../../application/use_cases/schedules/find-schedule-by-id')
-const CreateSchedule = require('../../application/use_cases/schedules/create-schedule')
-const DeleteSchedule = require('../../application/use_cases/schedules/delete-schedule')
-const UpdateSchedule = require('../../application/use_cases/schedules/update-schedule')
-// por enquanto os reposiories serão importados e passados para o use-case no controller
 const ScheduleRepository = require('../../infrastructure/repositories/schedule-repository')
 const Connection = require('../../infrastructure/database/connection')
 const Pool = require('../../infrastructure/database/pool')
@@ -18,50 +13,6 @@ class ScheduleController {
     const schedules = await allSchedules.execute()
 
     return schedules
-  }
-
-  async find(request) {
-    const schedulesRepository = new ScheduleRepository()
-    const findSchedulesById = new FindSchedulesById(schedulesRepository)
-    const { id } = request.params
-    const schedule = await findSchedulesById.execute(id)
-
-    return schedule
-  }
-
-  async create(request) {
-    const schedulesRepository = new ScheduleRepository()
-    const createSchedule = new CreateSchedule(schedulesRepository)
-    const schedule = await createSchedule.execute(request.payload)
-
-    return schedule
-  }
-
-  async delete(request, h) {
-    const schedulesRepository = new ScheduleRepository()
-    const deleteSchedule = new DeleteSchedule(schedulesRepository)
-    const { id } = request.params
-    const { message, code } = await deleteSchedule.execute(id)
-
-    return h.response({ message }).code(code)
-  }
-
-  async update(request, h) {
-    const schedulesRepository = new ScheduleRepository()
-
-    const updateSchedule = new UpdateSchedule(schedulesRepository)
-
-    const { id } = request.params
-    const { diaSemana, chegada, saida, ativo } = request.payload
-
-    const { message, code } = await updateSchedule.execute(id, {
-      diaSemana,
-      chegada,
-      saida,
-      ativo,
-    })
-
-    return h.response({ message }).code(code)
   }
 }
 
