59af56f00fa75cb5090224d3776630bf1599af1e
Joaquim Firmo <joaquimnt18@gmail.com>
2023-09-30 22:06:37 -0300

    teste.

--- infrastructure/database/pool.js
+++ infrastructure/database/pool.js
@@ -17,7 +17,7 @@ class Pool {
       password: this.password,
       port: this.port,
       max: 20,
-      idleTimeoutMillis: 3000,
+      idleTimeoutMillis: 30000,
       connectionTimeoutMillis: 2000,
     })
   }
