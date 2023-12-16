9406837ad4e4e0c7bc0a8ffc407cf6e28eb236a3
Joaquim Firmo <joaquimnt18@gmail.com>
2023-12-16 17:04:20 -0300

    feat: add possibilidade de busca de transportes por cidade

--- src/infrastructure/repositories/transports-repository.ts
+++ src/infrastructure/repositories/transports-repository.ts
@@ -27,14 +27,14 @@ export default class TransportsRepository implements ITransportsRepository {
       `SELECT horarios.dia_semana,
                 rotas.cidade_origem,
                 rotas.cidade_destino,
-                rotas.tipo_veiculo,
+                transportes.nome,
+                tipos_transportes.veiculo,
                 horarios.hora_saida,
-                horarios.hora_chegada,
-                tarifas.valor
-        FROM rotas
-            INNER JOIN rotas_horarios ON rotas.id = rotas_horarios.rotas_id
-            INNER JOIN horarios ON horarios.id = rotas_horarios.horarios_id
-            INNER JOIN tarifas ON  tarifas.id = rotas.tarifa_id
+                horarios.hora_chegada
+        FROM horarios
+            INNER JOIN rotas ON rotas.id = horarios.id_rota
+            INNER JOIN transportes ON transportes.id = horarios.id_transporte
+            INNER JOIN tipos_transportes ON tipos_transportes.id = transportes.id_tipo_transporte
         WHERE rotas.cidade_origem = $1;`,
       [city]
     )
--- src/interfaces/controllers/transports-controller.ts
+++ src/interfaces/controllers/transports-controller.ts
@@ -1,5 +1,6 @@
 import { Request, ResponseToolkit } from '@hapi/hapi'
 import ListAllTransports from '../../application/usecases/transports/list-transports'
+import findTransportsByCity from '../../application/usecases/transports/find-transports'
 import DatabaseRepositoryFactory from '../../infrastructure/factories/database-repository-factory'
 import Connection from '../../infrastructure/database/connection'
 
@@ -14,25 +15,23 @@ class TransportsController {
     request: Request,
     h: ResponseToolkit
   ): Promise<any> {
-    await this.connection.connect()
+    //  await this.connection.connect()
     const transportsRepository = new DatabaseRepositoryFactory(this.connection)
     const listAllTransports = new ListAllTransports(transportsRepository)
     const transports = await listAllTransports.execute()
     return h.response(transports).code(200)
   }
 
-  // public async findSchedulesByCity(
-  //   request: Request,
-  //   h: ResponseToolkit
-  // ): Promise<any> {
-  //   const pool = Pool.getInstance()
-  //   const connection = new Connection(pool)
-  //   const scheduleRepository = new ScheduleRepository(connection)
-  //   await connection.connect()
-  //   const findSchedules = new FindSchedules(scheduleRepository)
-  //   const schedules = await findSchedules.execute(request.params.city)
-  //   return h.response(schedules).code(200)
-  // }
+  public async findSchedulesByCity(
+    request: Request,
+    h: ResponseToolkit
+  ): Promise<any> {
+    await this.connection.connect()
+    const transportsRepository = new DatabaseRepositoryFactory(this.connection)
+    const transportsByCity = new findTransportsByCity(transportsRepository)
+    const transports = await transportsByCity.execute(request.params.city)
+    return h.response(transports).code(200)
+  }
 }
 
 export default new TransportsController()
