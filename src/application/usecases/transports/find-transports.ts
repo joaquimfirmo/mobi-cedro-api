304389452d2d2482cd49747b9f923aa84f49d29d
Joaquim Firmo <joaquimnt18@gmail.com>
2024-01-20 21:20:15 -0300

    refactor: rerefatoracao do transports-usecases

--- src/application/usecases/transports/find-transports.ts
+++ src/application/usecases/transports/find-transports.ts
@@ -1,18 +1,12 @@
-import 'reflect-metadata'
 import { Inject, Service } from 'typedi'
 import TransportsRepository from '../../repositories/transports-repository'
-import RepositoryFactory from '../../factories/repository-factory'
 
 @Service('usecase.findTransportsByCity')
 export default class FindTransportsByCity {
-  transportsRepository: TransportsRepository
   constructor(
-    @Inject('transportsRepositoryFactory')
-    transportsRepositoryFactory: RepositoryFactory
-  ) {
-    this.transportsRepository =
-      transportsRepositoryFactory.createTransportsRepository()
-  }
+    @Inject('repository-transports')
+    readonly transportsRepository: TransportsRepository
+  ) {}
 
   async execute(city: string): Promise<any> {
     const transports = await this.transportsRepository.findByCity(city)
--- src/application/usecases/transports/list-transports.ts
+++ src/application/usecases/transports/list-transports.ts
@@ -1,19 +1,12 @@
-import 'reflect-metadata'
 import { Inject, Service } from 'typedi'
 import TransportsRepository from '../../repositories/transports-repository'
-import RepositoryFactory from '../../factories/repository-factory'
 
 @Service('usecase.listAllTransports')
 export default class ListAllTransports {
-  transportsRepository: TransportsRepository
-
   constructor(
-    @Inject('transportsRepositoryFactory')
-    transportsRepositoryFactory: RepositoryFactory
-  ) {
-    this.transportsRepository =
-      transportsRepositoryFactory.createTransportsRepository()
-  }
+    @Inject('repository-transports')
+    readonly transportsRepository: TransportsRepository
+  ) {}
 
   async execute(): Promise<any> {
     const transports = await this.transportsRepository.findAll()
