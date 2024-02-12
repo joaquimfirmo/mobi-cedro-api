e4ddd93ddd889e66377ae1ab64b514535a26b00f
Joaquim Firmo <joaquimnt18@gmail.com>
2024-02-11 21:30:48 -0300

    feat: remove entitie routes

--- src/domain/entities/routes.ts
+++
@@ -1,8 +0,0 @@
-export default class Route {
-  constructor(
-    public readonly id: string,
-    public readonly cidade_origem: string,
-    public readonly cidade_destino: string,
-    public readonly nome: string
-  ) {}
-}
