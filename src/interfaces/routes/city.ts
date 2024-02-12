745fe080e0f651e18b9629fe35c764799b5b1caa
Joaquim Firmo <joaquimnt18@gmail.com>
2024-02-11 21:35:21 -0300

    feat: cria city-routes

---
+++ src/interfaces/routes/city.ts
@@ -0,0 +1,22 @@
+import { Container } from 'typedi'
+import CityController from '../controllers/city-controller'
+const cityController = Container.get(CityController)
+
+module.exports = {
+  name: 'city',
+  version: '1.0.0',
+  register: async function (server: any) {
+    server.route([
+      {
+        method: 'POST',
+        path: '/cidade',
+        handler: cityController.create.bind(cityController),
+      },
+      {
+        method: 'GET',
+        path: '/cidades',
+        handler: cityController.findAll.bind(cityController),
+      },
+    ])
+  },
+}
