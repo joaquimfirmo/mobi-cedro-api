0247be064537a20ee8394003c79fb5f70df7e51c
Joaquim Firmo <joaquimnt18@gmail.com>
2024-02-25 22:51:58 -0300

    feat: cria metodos delete eupdate  no company-controller

--- src/interfaces/controllers/company-controller.ts
+++ src/interfaces/controllers/company-controller.ts
@@ -2,6 +2,8 @@ import { Inject, Service } from 'typedi'
 import { Request, ResponseToolkit } from '@hapi/hapi'
 import CreateCompany from '../../application/usecases/company/create-company'
 import FindAllCompany from '../../application/usecases/company/findAll-company'
+import UpdateCompany from '../../application/usecases/company/update-company'
+import DeleteCompany from '../../application/usecases/company/delete-company'
 
 @Service()
 export default class CompanyController {
@@ -9,7 +11,11 @@ export default class CompanyController {
     @Inject('usecase.createCompany')
     private readonly createCompany: CreateCompany,
     @Inject('usecase.findAllCompany')
-    private readonly findAllCompany: FindAllCompany
+    private readonly findAllCompany: FindAllCompany,
+    @Inject('usecase.updateCompany')
+    private readonly updateCompany: UpdateCompany,
+    @Inject('usecase.deleteCompany')
+    private readonly deleteCompany: DeleteCompany
   ) {}
 
   async create(request: Request, h: ResponseToolkit): Promise<any> {
@@ -34,5 +40,30 @@ export default class CompanyController {
     const { limit, offset } = request.query as any
     const result = await this.findAllCompany.execute(limit, offset)
     return h.response(result).code(200)
+  }
+
+  async update(request: Request, h: ResponseToolkit): Promise<any> {
+    const id = request.params.id
+    const { companyUpdated, message, status } =
+      await this.updateCompany.execute(id, request.payload)
+
+    return h
+      .response({
+        companyUpdated,
+        message,
+      })
+      .code(status)
+  }
+
+  async delete(request: Request, h: ResponseToolkit): Promise<any> {
+    const id = request.params.id
+    const { message, status, data } = await this.deleteCompany.execute(id)
+
+    return h
+      .response({
+        message,
+        data,
+      })
+      .code(status)
   }
 }
