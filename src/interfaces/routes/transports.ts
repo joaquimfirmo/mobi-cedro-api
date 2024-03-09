7d3d6c59c04644eafc96ed9dbfd65ebed2d65e18
Joaquim Firmo <joaquimnt18@gmail.com>
2024-03-09 18:14:36 -0300

    feat:add validacao de payload e parametros nas rotas de transportes.

--- src/interfaces/routes/transports.ts
+++ src/interfaces/routes/transports.ts
@@ -1,4 +1,5 @@
 import { Container } from 'typedi'
+import Joi from 'joi'
 import TransportsController from '../controllers/transports-controller'
 const transportsController = Container.get(TransportsController)
 
@@ -16,6 +17,13 @@ module.exports = {
       {
         method: 'GET',
         path: '/transportes/cidade/{cityId}',
+        options: {
+          validate: {
+            params: Joi.object({
+              cityId: Joi.string().guid().required(),
+            }),
+          },
+        },
         handler:
           transportsController.getTransportsByCity.bind(transportsController),
       },
@@ -23,6 +31,43 @@ module.exports = {
       {
         method: 'POST',
         path: '/transporte',
+        options: {
+          validate: {
+            payload: Joi.object({
+              cidade_origem: Joi.string().min(2).max(50).required(),
+              cidade_destino: Joi.string().min(2).max(50).required(),
+              dia_semana: Joi.string()
+                .min(2)
+                .max(50)
+                .valid(
+                  'Segunda-Feira',
+                  'Terça-Feira',
+                  'Quarta-Feira',
+                  'Quinta-Feira',
+                  'Sexta-Feira',
+                  'Sábado',
+                  'Domingo'
+                )
+                .required(),
+              localizacao: Joi.string().min(2).max(50).required(),
+              hora_saida: Joi.string()
+                // eslint-disable-next-line no-useless-escape
+                .pattern(/^([0-9]{2})\:([0-9]{2})\:([0-9]{2})$/)
+                .required(),
+              hora_chegada: Joi.string()
+                // eslint-disable-next-line no-useless-escape
+                .pattern(/^([0-9]{2})\:([0-9]{2})\:([0-9]{2})$/)
+                .required(),
+              preco: Joi.string()
+                .pattern(/^\$\d+(?:\.\d{0,2})$/)
+                .required(),
+              nome: Joi.string().min(2).max(50).required(),
+              id_empresa: Joi.string().guid().required(),
+              id_veiculo: Joi.string().guid().required(),
+              id_cidade: Joi.string().guid().required(),
+            }),
+          },
+        },
         handler:
           transportsController.createTransport.bind(transportsController),
       },
@@ -35,6 +80,13 @@ module.exports = {
       {
         method: 'DELETE',
         path: '/transportes/{id}',
+        options: {
+          validate: {
+            params: Joi.object({
+              id: Joi.string().guid().required(),
+            }),
+          },
+        },
         handler:
           transportsController.deleteTransportById.bind(transportsController),
       },
