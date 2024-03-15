3a396d68c3fb86388533b5caaf6e4df2025804a1
Joaquim Firmo <joaquimnt18@gmail.com>
2024-03-15 17:46:59 -0300

    refactor:altjuste messagem de erro de validacao do paramDto.

--- src/application/dto/param-dto.ts
+++ src/application/dto/param-dto.ts
@@ -1,6 +1,6 @@
 import { IsUUID } from 'class-validator'
 
 export default class ParamDto {
-  @IsUUID(4, { message: 'Id inválido' })
+  @IsUUID(4, { message: 'Id é um UUID-V4 inválido' })
   id: string
 }
