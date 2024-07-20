c0a80a63f6f7137da9aad443f0da9f97bb69cd89
Joaquim Firmo <joaquimnt18@gmail.com>
2024-07-20 16:14:48 -0300

    refactor: remove metodo de validacao de municipios

--- src/domain/entities/city.ts
+++ src/domain/entities/city.ts
@@ -1,5 +1,4 @@
 import crypto from 'crypto'
-import municipios from './../../utils/municipios.json'
 import { IsString, MinLength, MaxLength } from 'class-validator'
 
 export default class City {
@@ -17,20 +16,10 @@ export default class City {
     this.id = id
     this.nome = nome
     this.uf = uf
-    this.validateCity(nome, uf)
   }
 
   static create(nome: string, uf: string): City {
     const id = crypto.randomUUID()
     return new City(id, nome, uf)
-  }
-
-  validateCity(nome: string, uf: string): void {
-    const city = municipios.data.find(
-      (municipio) => municipio.Nome === nome && municipio.Uf === uf
-    )
-    if (!city) {
-      throw new Error('Cidade inválida')
-    }
   }
 }
